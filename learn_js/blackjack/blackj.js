6ea0cbd2b39866eeb029be2a5fe8936a5761f413
Myriad Hero <kddreauw@gmail.com>
2022-03-13 12:06:04 +1100

    blackj progress

--- learn_js/blackjack/blackj.js
+++ learn_js/blackjack/blackj.js
@@ -1,4 +1,33 @@
+class Card{
+    constructor(suit, face){
+        this.isAce = face==="Ace";
+        this.value = (typeof face)==='number' ? face : (this.isAce ? 11 : 10);
+        this.suit = suit;
+    }
+}
 
+class Deck{
+    constructor(numberOfDecks=1){
+        this.deck = [];
+        for (let i = 0; i < numberOfDecks; i++) {
+            for (let suit of ['Spades', 'Hearts', 'Clubs', 'Dimonds']){
+                for (let face of [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace']){
+                    this.deck.push(new Card(suit,face));
+                }
+            }
+        }
+    }
+
+    takeTop(){
+        return this.deck.pop();
+    }
+
+    shuffle(){
+        this.deck = this.deck.map(card=>({c:card, sort:Math.random()}))
+                    .sort((a,b)=>(a.sort-b.sort))
+                    .map(({card}) => card);
+    }
+}
 
 let startBtn = document.getElementById("start-btn");
 let cardsElem = document.getElementById("cards");
