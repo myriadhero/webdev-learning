e179bbb5d151ec51987bd4f7a49600fcf2956a22
Kirill Duplyakin <kduplyakin@atlassian.com>
2022-03-13 17:29:15 +1100

    blackjack progress

--- learn_js/blackjack/blackj.js
+++ learn_js/blackjack/blackj.js
@@ -1,6 +1,7 @@
 class Card{
     constructor(suit, face){
         this.isAce = face==="Ace";
+        this.face = face;
         this.value = (typeof face)==='number' ? face : (this.isAce ? 11 : 10);
         this.suit = suit;
     }
@@ -23,32 +24,85 @@ class Deck{
     }
 
     shuffle(){
-        this.deck = this.deck.map(card=>({c:card, sort:Math.random()}))
+        this.deck = this.deck.map(card=>({card, sort:Math.random()}))
                     .sort((a,b)=>(a.sort-b.sort))
                     .map(({card}) => card);
     }
 }
 
+let startWrapper = document.getElementById("start-wrapper");
 let startBtn = document.getElementById("start-btn");
-let cardsElem = document.getElementById("cards");
-let sumElem = document.getElementById("sum");
 
-startBtn.onclick = gameRound2Cards;
+let gameWrapper = document.getElementById("game-wrapper");
+let dCardsElem = document.getElementById("dcards");
+let dPointsElem = document.getElementById("dpoints");
+let pCardsElem = document.getElementById("pcards");
+let pPointsElem = document.getElementById("ppoints");
+let hitBtn = document.getElementById("hit-btn");
+let standBtn = document.getElementById("stand-btn");
 
-function gameRound2Cards(){
-    let firstCard,secondCard;
-    let isAlive = true;
+startBtn.onclick = startGame;
+hitBtn.onclick = hit;
+standBtn.onclick = stand;
 
-    firstCard = dealOne(); 
-    secondCard = dealOne();
-    cardsElem.innerText = `Cards: `;
-    let sum = firstCard+secondCard;
+let deck;
+let dcards, dpoints, pcards, ppoints;
 
-    let hasBlackJ = sum===21;
+function startGame(){
+    startWrapper.style.display = "none";
+    gameWrapper.style.display = 'block';
+    deck = new Deck;
+    deck.shuffle();
+    // deal player
+    pcards = [deck.takeTop(), deck.takeTop()];
+    // deal dealer
+    dcards = [deck.takeTop(), deck.takeTop()];
+
+    // calc points
+    dpoints = calcPoints([dcards[0]]);
+    ppoints = calcPoints(pcards);
+
+    dPointsElem.innerText = `Dealer Points: ${dpoints}*`;
+    pPointsElem.innerText = `Player Points: ${ppoints}`;
+    
+    pCardsElem.innerText = `Player cards: ${stringCards(pcards)}`;
+    dCardsElem.innerText = `Dealer cards: ${stringCards([dcards[0]])}, *`;
+    
+    dpoints = calcPoints(dcards);
+    if (dpoints === 21 ){
+        ppoints === 21 ? draw() : dealerWon();
+    }
+    else if (ppoints===21){
+        playerWon();
+    }
 
 }
 
+function calcPoints(cards){
+    let total = 0;
+    for (let card of cards){
+        // naive first ace is 11 others 1
+        if (!card.isAce){
+            total += card.value;
+        }
+        else if (total+11>21){
+            total+=1;
+        }
+        else{
+            total+=11;
+        }
+    }
+    return total;
+}
 
-function dealOne(){
-    return Math.round(Math.random()*9)+2;
+function stringCards(cards){
+    return cards.map(card=>card.face+" of "+card.suit).join(", ");
+}
+
+function hit(){
+
+}
+
+function stand(){
+
 }
