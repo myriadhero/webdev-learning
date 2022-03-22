91c993fef517b0830e9977b0537818f8b038ea69
Myriad Hero <kddreauw@gmail.com>
2022-03-22 12:09:11 +1100

    start snake

---
+++ learn_js/snake/snake.js
@@ -0,0 +1,8 @@
+const [WIDTH,HEIGHT] = [50,50];
+
+
+
+let canvElem = document.getElementById("snake-canvas");
+canvElem.width = 500;
+canvElem.height = 500;
+let context = canvElem.getContext("2d");
